3b3ac41671d9e93de2416e7dace2c12b8b863c2a
unknown <nabilnasser850@gmail.com>
2022-12-28 01:31:00 +0200

    first commit

---
+++ src/Soldier/Anim.java
@@ -0,0 +1,45 @@
+package Soldier;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package project;
+
+import com.sun.opengl.util.Animator;
+import com.sun.opengl.util.FPSAnimator;
+
+import javax.media.opengl.GLCanvas;
+import javax.swing.*;
+import java.awt.*;
+
+public class Anim extends JFrame {
+
+    public static void main(String[] args) {
+        new Anim();
+    }
+
+
+    public Anim() {
+        GLCanvas glcanvas;
+        Animator animator;
+        
+        AnimListener listener = new AnimGLEventListener3();
+        glcanvas = new GLCanvas();
+        glcanvas.addGLEventListener(listener);
+        glcanvas.addKeyListener(listener);
+        getContentPane().add(glcanvas, BorderLayout.CENTER);
+        animator = new FPSAnimator(15);
+        animator.add(glcanvas);
+        animator.start();
+
+        setTitle("Anim Test");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(700, 700);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setFocusable(true);
+        glcanvas.requestFocus();
+    }
+}
